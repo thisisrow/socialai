bef8e4796b8eb1efdd1c6b3e356335c45e0f7135
thisisrow <prathameshmishra2020@gmail.com>
2026-01-29 07:35:21 +0000

    ngrul

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 const APP_ID = "1251511386469731";
 const REDIRECT_URI = "https://socialai-theta.vercel.app/";
-const API_BASE = "https://757444bf5ddc.ngrok-free.app";
+const API_BASE = "https://e68e1230eb96.ngrok-free.app";
 
 const scopes = [
   "instagram_business_basic",
