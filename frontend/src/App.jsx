f76a4af73710220c8f5a20416a4c5164b3bb1477
thisisrow <prathameshmishra2020@gmail.com>
2026-01-29 07:47:58 +0000

    ulr

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 const APP_ID = "1251511386469731";
 const REDIRECT_URI = "https://socialai-theta.vercel.app/";
-const API_BASE = "https://b2dc475905ff.ngrok-free.app";
+const API_BASE = "https://server.palgharcity48.com";
 
 const scopes = [
   "instagram_business_basic",
