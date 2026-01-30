44d92f054c3ad36fd830db86f40e246ad5a67abc
thisisrow <prathameshmishra2020@gmail.com>
2026-01-30 16:03:22 +0530

    Update API_BASE URL for the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 const APP_ID = "1251511386469731";
 const REDIRECT_URI = "https://socialai-theta.vercel.app/";
-const API_BASE = "https://6eac75832507.ngrok-free.app";
+const API_BASE = "https://40ee0a2243dd.ngrok-free.app";
 
 const scopes = [
   "instagram_business_basic",
